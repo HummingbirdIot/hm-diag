b7495a89d54034f70cbe4635a0f19e11560a5f31
hummingbirdiot <hummingbirdiot@outlook.com>
2021-10-20 17:22:04 +0800

    Fix deb build

--- build.sh
+++ build.sh
@@ -17,8 +17,9 @@ sed -i "s/{version}/${version}/g" ./build-deb/DEBIAN/control
 
 go build
 
+mkdir -p ./build-deb/usr/local/bin
 rm -fr ./build-deb/usr/local/bin/*
 cp hm-diag ./build-deb/usr/local/bin
 
-dpkg -b ./build-deb hm-diag_${GOOS}_${GOARCH}.deb
+dpkg-deb --root-owner-group --build ./build-deb hm-diag_${GOOS}_${GOARCH}.deb
 mv hm-diag hm-diag_${GOOS}_${GOARCH}
